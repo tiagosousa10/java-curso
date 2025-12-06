0b384a33a2808ff4e6ff8cb90e1e5a7c58b2226c
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-12-06 11:14:51 +0000

    array elements

---
+++ meu-primeiro-programa/src/InicioArrays.java
@@ -0,0 +1,24 @@
+public class InicioArrays {
+    public static void main(String[] args) {
+
+        String[] filhos = {"Ze", "Tiago", "Joana"};
+
+        int[] numerosImpares = {1,3,5,7,9};
+        System.out.println(filhos[0]);
+        System.out.println(filhos[1]);
+        System.out.println(filhos[2]);
+        System.out.println(filhos[1]);
+        System.out.println(filhos[0]);
+
+        String nomePrimeiroFilho = filhos[0];
+        String nomeSegundoFilho = filhos[1];
+
+        String[] telefones = new String[2];
+        telefones[0] = "123123123";
+        telefones[1] = "321321321";
+
+        System.out.println(telefones[0]);
+        System.out.println(telefones[1]);
+
+    }
+}
