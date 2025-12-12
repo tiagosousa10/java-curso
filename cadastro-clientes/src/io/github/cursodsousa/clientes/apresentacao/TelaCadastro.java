a084edc033250112ce6f330b8206ddb5a2d665a9
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-12-12 15:37:08 +0000

    first components for Tela Cadastro

--- cadastro-clientes/src/io/github/cursodsousa/clientes/apresentacao/TelaCadastro.java
+++ cadastro-clientes/src/io/github/cursodsousa/clientes/apresentacao/TelaCadastro.java
@@ -8,6 +8,10 @@ public class TelaCadastro extends JFrame {
     private JLabel labelCpf;
     private JLabel labelSexo;
 
+    private JTextField campoNome;
+    private JTextField campoCpf;
+    private JComboBox<String> campoSexo;
+
     public TelaCadastro() {
         construirTela();
     }
@@ -18,6 +22,46 @@ public class TelaCadastro extends JFrame {
         setTitle("Cadastro de Clientes");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLayout(null);
+
+
+        this.adicionarCampos();
+        this.adicionarBotoes();
+        this.adicionarComponentesFoto();
+    }
+
+    private void adicionarCampos() {
+        labelNome = new JLabel("Nome:");
+        labelNome.setBounds(20,20,200,20);
+        getContentPane().add(labelNome);
+
+        campoNome = new JTextField();
+        campoNome.setBounds(20,40,200,20);
+        getContentPane().add(campoNome);
+
+        labelCpf = new JLabel("CPF:");
+        labelCpf.setBounds(20,60,200,20);
+        getContentPane().add(labelCpf);
+
+        campoCpf = new JTextField();
+        campoCpf.setBounds(20,80,200,20);
+        getContentPane().add(campoCpf);
+
+        labelSexo = new JLabel("Sexo:");
+        labelSexo.setBounds(20,100,200,20);
+        getContentPane().add(labelSexo);
+
+
+        String[] tiposSexo = {"M", "F", "O"};
+        campoSexo = new JComboBox<>(tiposSexo);
+        campoSexo.setBounds(20,120,200,20);
+        getContentPane().add(campoSexo);
+    }
+
+    private void adicionarBotoes(){
+
+    }
+
+    private void adicionarComponentesFoto() {
 
     }
 
