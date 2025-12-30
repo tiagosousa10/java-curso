56c396599c939db0674ec29e313ebf04daea92ab
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-12-30 14:56:12 +0000

    mapping organizacao clientes

--- cadastro-clientes/src/io/github/cursodsousa/clientes/colecoes/Main.java
+++ cadastro-clientes/src/io/github/cursodsousa/clientes/colecoes/Main.java
@@ -8,6 +8,46 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
+        OrganizacaoClientes cadastro = new OrganizacaoClientes();
+        Cliente cliente = new Cliente();
+        cliente.setCpf("0123");
+        cliente.setNome("Rio Grande");
+        cliente.setSexo(TipoSexo.M);
+
+        cadastro.adicionar(cliente);
+
+        Cliente cliente2 = new Cliente();
+        cliente2.setCpf("0123");
+        cliente2.setNome("Maria");
+        cliente2.setSexo(TipoSexo.F);
+
+        cadastro.adicionar(cliente2);
+
+        Cliente cliente3 = new Cliente();
+        cliente3.setCpf("0123");
+        cliente3.setNome("Rita");
+        cliente3.setSexo(TipoSexo.F);
+
+        cadastro.adicionar(cliente3);
+
+        Cliente cliente4 = new Cliente();
+        cliente4.setCpf("0123");
+        cliente4.setNome("Alguem");
+        cliente4.setSexo(TipoSexo.O);
+
+        cadastro.adicionar(cliente3);
+
+        System.out.println("Femininos: ");
+        cadastro.imprimirClientes(TipoSexo.F);
+
+        System.out.println("Masculinos: ");
+        cadastro.imprimirClientes(TipoSexo.M);
+
+        System.out.println("Outros: ");
+        cadastro.imprimirClientes(TipoSexo.O);
+    }
+
+    private static void exemplosMaps() {
         Map<Integer,String> mapa = new HashMap<>();
         mapa.put(1,"Fulano");
         mapa.put(2,"Cicrano");
@@ -23,7 +63,6 @@ public class Main {
         boolean contemChave = mapa.containsKey(1);
 
         System.out.println("Contem a chave 1?: " + contemChave);
-
     }
 
     private static void TrabalhandoComOrdenacaoDeColecoes() {
--- cadastro-clientes/src/io/github/cursodsousa/clientes/colecoes/OrganizacaoClientes.java
+++ cadastro-clientes/src/io/github/cursodsousa/clientes/colecoes/OrganizacaoClientes.java
@@ -1,4 +1,50 @@
 package io.github.cursodsousa.clientes.colecoes;
 
+import io.github.cursodsousa.clientes.dominio.Cliente;
+import io.github.cursodsousa.clientes.dominio.enums.TipoSexo;
+
+import java.util.*;
+
 public class OrganizacaoClientes {
+    private Map<TipoSexo, List<Cliente>> mapa;
+
+
+    public OrganizacaoClientes() {
+        this.mapa = new HashMap<>();
+    }
+
+    public void adicionar(Cliente cliente) {
+        boolean jaContemSexo = mapa.containsKey(cliente.getSexo());
+
+        if (jaContemSexo) {
+            List<Cliente> clientes = mapa.get(cliente.getSexo());
+            clientes.add(cliente);
+        } else {
+            List<Cliente> clientes = new ArrayList<>();
+            clientes.add(cliente);
+            mapa.put(cliente.getSexo(), clientes);
+        }
+    }
+
+    public List<Cliente> buscarPorSexo(TipoSexo tipo) {
+        return mapa.get(tipo);
+    }
+
+    public List<TipoSexo> obterTiposAdicionados() {
+        Set<TipoSexo> tipoSexos = mapa.keySet();
+        return new ArrayList<>(tipoSexos);
+    }
+
+    public void imprimirClientes(TipoSexo tipo) {
+        List<Cliente> clientes = mapa.get(tipo);
+
+        if(clientes != null) {
+            for(Cliente c : clientes) {
+                System.out.println(c);
+            }
+        } else {
+            System.out.println("Lista Vazia");
+        }
+
+    }
 }
