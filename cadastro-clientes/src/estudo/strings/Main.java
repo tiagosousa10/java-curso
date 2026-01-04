d2c96430aec8920a2a4ae27874e703149f3cd781
Tiago Sousa <tiagosousa.tams@hotmail.com>
2026-01-04 16:47:48 +0000

    regex expressios

--- cadastro-clientes/src/estudo/strings/Main.java
+++ cadastro-clientes/src/estudo/strings/Main.java
@@ -6,12 +6,22 @@ public class Main {
     static String valor = "Arroz";
 
     public static void main(String[] args) {
-        tamanhoString();
-        pegarCaracteres();
-        trabalhandoComSubstring();
-        exemploIndexOf();
-        exemplosCase();
-        removerEspacos();
+        String hello = "Hello";
+
+        System.out.println("Parece com hello "+ hello.equalsIgnoreCase("heLLo"));
+        System.out.println("Começa com H?" + hello.startsWith("He"));
+        System.out.println("Acaba com H?" + hello.endsWith("lo"));
+
+        String nomeCompleto = "Tiago Sousa";
+        String[] partesDoNome = nomeCompleto.split("");
+        System.out.println(partesDoNome[2]);
+//        tamanhoString();
+//        pegarCaracteres();
+//        trabalhandoComSubstring();
+//        exemploIndexOf();
+//        exemplosCase();
+//        removerEspacos();
+//        replace();
     }
 
     private static void tamanhoString() {
@@ -21,13 +31,13 @@ public class Main {
 
     private static void pegarCaracteres() {
         char caractere = valor.charAt(0);
-        char ultimo = valor.charAt(valor.length() -1);
+        char ultimo = valor.charAt(valor.length() - 1);
         System.out.println("primeiro caractere: " + caractere + " e " + ultimo);
     }
 
     private static void trabalhandoComSubstring() {
-    String pedaco = valor.substring(0, 2);
-    System.out.println(pedaco);
+        String pedaco = valor.substring(0, 2);
+        System.out.println(pedaco);
     }
 
     private static void exemploIndexOf() {
@@ -40,7 +50,7 @@ public class Main {
 
         System.out.println(("Indice encontrado: " + indice));
 
-        if(nome.indexOf("Sousa") != -1){
+        if (nome.indexOf("Sousa") != -1) {
             System.out.println("Encontrei! + " + nome);
 
         } else {
@@ -58,7 +68,7 @@ public class Main {
         String minuscula = string.toLowerCase();
         System.out.println(minuscula);
 
-        System.out.println("String original: "+ string);
+        System.out.println("String original: " + string);
 
     }
 
@@ -67,5 +77,13 @@ public class Main {
         String semEspacos = string.trim();
         System.out.println(semEspacos);
         System.out.println(string.length());
+    }
+
+    public static void replace() {
+        String novaMaca = valor.replace("A", "K").replace("r", "T");
+        System.out.println(novaMaca);
+
+        String cpf = "012.345.678-90";
+        System.out.println(cpf.replace(".", "").replace("-",""));
     }
 }
